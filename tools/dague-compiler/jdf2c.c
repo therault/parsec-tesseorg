8af0b0d8bab4324edcc2013339523faff31b8e57
Mathieu Faverge <mathieu.faverge@inria.fr>
2016-05-02 11:42:06 -0400

    Fix DAGUE_SIM compilation

--- tools/dague-compiler/jdf2c.c
+++ tools/dague-compiler/jdf2c.c
@@ -2709,10 +2709,11 @@ static void jdf_generate_simulation_cost_fct(const jdf_t *jdf, const jdf_functio
     coutput("#if defined(DAGUE_SIM)\n"
             "static int %s(const %s *this_task)\n"
             "{\n"
-            "  const dague_handle_t *__dague_handle = (const dague_handle_t*)this_task->dague_handle;\n"
+            "  const __dague_%s_internal_handle_t *__dague_handle = (const __dague_%s_internal_handle_t *)this_task->dague_handle;\n"
             "%s"
             "  (void)__dague_handle;\n",
             prefix, dague_get_name(jdf, f, "task_t"),
+            jdf_basename, jdf_basename,
             UTIL_DUMP_LIST(sa1, f->locals, next,
                            dump_local_assignments, &ai, "", "  ", "\n", "\n"));
 
@@ -4498,7 +4499,7 @@ static void jdf_generate_code_hook_cuda(const jdf_t *jdf,
                 fl->varname);
     }
     coutput("    if( this_task->function->sim_cost_fct != NULL ) {\n"
-            "      this_task->sim_exec_date += this_task->function->sim_cost_fct(this_task);\n"
+            "      this_task->sim_exec_date += this_task->function->sim_cost_fct((dague_execution_context_t*)this_task);\n"
             "    }\n"
             "    if( context->largest_simulation_date < this_task->sim_exec_date )\n"
             "      context->largest_simulation_date = this_task->sim_exec_date;\n"
@@ -4783,7 +4784,7 @@ static void jdf_generate_code_hook(const jdf_t *jdf,
                 fl->varname);
     }
     coutput("    if( this_task->function->sim_cost_fct != NULL ) {\n"
-            "      this_task->sim_exec_date += this_task->function->sim_cost_fct(this_task);\n"
+            "      this_task->sim_exec_date += this_task->function->sim_cost_fct((dague_execution_context_t*)this_task);\n"
             "    }\n"
             "    if( context->largest_simulation_date < this_task->sim_exec_date )\n"
             "      context->largest_simulation_date = this_task->sim_exec_date;\n"
