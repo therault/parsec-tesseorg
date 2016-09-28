26b7c031d574368afa794136d2690abf08c2821f
Damien Genet <dgenet@icl.utk.edu>
2016-09-28 12:46:37 -0400

    Adds: summa jdf, irregular_tiled_matrix

---
+++ summa/include/irregular_tiled_matrix.h
@@ -0,0 +1,132 @@
+
+
+
+#ifndef _TILE_COLLECTION_H_
+#define _TILE_COLLECTION_H_
+
+#include "dague_config.h"
+#include <assert.h>
+#include "precision.h"
+#include "dague/data.h"
+
+enum tile_coll_type {
+    tile_coll_Byte          = 0, /**< unsigned char  */
+    tile_coll_Integer       = 1, /**< signed int     */
+    tile_coll_RealFloat     = 2, /**< float          */
+    tile_coll_RealDouble    = 3, /**< double         */
+    tile_coll_ComplexFloat  = 4, /**< complex float  */
+    tile_coll_ComplexDouble = 5  /**< complex double */
+};
+
+static inline int dague_datadist_getsizeoftype(enum tile_coll_type type)
+{
+    switch( type ) {
+    case tile_coll_Byte          : return sizeof(char);
+    case tile_coll_Integer       : return sizeof(int);
+    case tile_coll_RealFloat     : return sizeof(float);
+    case tile_coll_RealDouble    : return sizeof(double);
+    case tile_coll_ComplexFloat  : return sizeof(dague_complex32_t);
+    case tile_coll_ComplexDouble : return sizeof(dague_complex64_t);
+    default:
+        return -1;
+    }
+}
+
+/**
+ * Convert from a matrix type to a more traditional PaRSEC type usable for
+ * creating arenas.
+ */
+static inline int dague_translate_matrix_type( enum tile_coll_type mt, dague_datatype_t* dt )
+{
+    switch(mt) {
+    case tile_coll_Byte:          *dt = dague_datatype_int8_t; break;
+    case tile_coll_Integer:       *dt = dague_datatype_int32_t; break;
+    case tile_coll_RealFloat:     *dt = dague_datatype_float_t; break;
+    case tile_coll_RealDouble:    *dt = dague_datatype_double_t; break;
+    case tile_coll_ComplexFloat:  *dt = dague_datatype_complex_t; break;
+    case tile_coll_ComplexDouble: *dt = dague_datatype_double_complex_t; break;
+    default:
+        fprintf(stderr, "%s:%d Unknown tile_coll_type (%d)\n", __func__, __LINE__, mt);
+        return -1;
+    }
+    return 0;
+}
+
+#define irregular_tiled_matrix_desc_type 0x10
+
+typedef struct irregular_tile_data_s {
+    dague_data_t super;
+#if defined(DAGUE_DEBUG)
+  uint64_t magic; /*constant to assert cast went as planned*/
+#endif
+    int           rank;
+    int           vpid;
+
+    int tileld;         /**< leading dimension of each tile (Should be a function depending on the row) */
+    int mb;             /**< number of rows in a tile */
+    int nb;             /**< number of columns in a tile */
+  void *data;           /**< pointer to the tile data */
+} irregular_tile_data_t;
+
+DAGUE_DECLSPEC dague_data_t* irregular_tile_data_new(void);
+
+
+
+typedef struct irregular_tiled_matrix_desc_s {
+    dague_ddesc_t super;
+    irregular_tile_data_t**       data_map;   /**< map of the data */
+  unsigned int * itiling; /**< array of size lmt+1 giving the range of indices of tile t in [itiling(t);itiling(t+1)]*/
+  unsigned int * jtiling; /**< array of size lnt+1 giving the range of indices of tile t in [jtiling(t);jtiling(t+1)]*/
+    enum matrix_type     mtype;      /**< precision of the matrix */
+    enum matrix_storage  storage;    /**< storage of the matrix   */
+    int dtype;          /**< Distribution type of descriptor      */
+    int bsiz;           /**< size in elements including padding of a tile - derived parameter */
+    int lm;             /**< number of rows of the entire matrix */
+    int ln;             /**< number of columns of the entire matrix */
+    int lmt;            /**< number of tile rows of the entire matrix */
+    int lnt;            /**< number of tile columns of the entire matrix */
+    int llm;            /**< number of rows of the matrix stored localy - derived parameter */
+    int lln;            /**< number of columns of the matrix stored localy - derived parameter */
+    int i;              /**< row tile index to the beginning of the submatrix */
+    int j;              /**< column tile index to the beginning of the submatrix */
+    int m;              /**< number of rows of the submatrix - derived parameter */
+    int n;              /**< number of columns of the submatrix - derived parameter */
+    int mt;             /**< number of tile rows of the submatrix - derived parameter */
+    int nt;             /**< number of tile columns of the submatrix - derived parameter */
+    int nb_local_tiles; /**< number of tile handled locally */
+} irregular_tiled_matrix_desc_t;
+
+void tiled_matrix_init(irregular_tiled_matrix_desc_t* ddesc,
+		       enum matrix_type mtype,
+		       unsigned int nodes, unsigned int myrank,
+		       unsigned int lm, unsigned int ln,
+		       unsigned int lmt, unsigned int lnt, 
+		       unsigned int*itiling, unsigned int* jtiling,
+		       unsigned int i, unsigned int j);
+/* add a parameter for number of expected tiles to register?*/
+/* I could then do a collective operation to to build the structure of the matrix */
+/* and the tiling vectors can be infered by sharing information */
+
+void tiled_matrix_destroy(irregular_tiled_matrix_desc_t* ddesc);
+
+/* i, j are tile coordinates, nb, mb are tile sizes */
+void tiled_matrix_set_data(irregular_tiled_matrix_desc_t *ddesc, void *actual_data, int i, int j, int vpid, int rank);
+
+void tiled_matrix_build(irregular_tiled_matrix_t *ddesc);
+
+#  if 0
+/* Alternative */
+void tiled_matrix_init(irregular_tiled_matrix_desc_t* ddesc,
+		       enum matrix_type mtype,
+		       unsigned int nodes, unsigned int myrank,
+		       unsigned int lm, unsigned int ln,
+		       unsigned int lmt, unsigned int lnt, 
+		       unsigned int i, unsigned int j,
+		       int local_tiles);
+
+/* when inserting the local_tiles-th element of the matrix, this call becomes a collective operation with information sharing to discover the tiling after insertion of the whole matrix */
+void tiled_matrix_set_data(irregular_tiled_matrix_desc_t *ddesc, void *actual_data, int i, int j, unsigned int mb, unsigned int nb, int vpid, int rank);
+
+#  endif
+
+#endif /* _TILE_COLLECTION_H_ */
---
+++ summa/lib/zgemm_wrapper.c
@@ -0,0 +1,377 @@
+/*
+ * Copyright (c) 2016      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * $COPYRIGHT
+ *
+ * @precisions normal z -> s d c
+ *
+ */
+
+/* #include "dplasma.h" */
+/* #include "dplasma/lib/dplasmatypes.h" */
+/* #include "data_dist/matrix/two_dim_rectangle_cyclic.h" */
+
+#include "irregular_tiled_matrix.h"
+
+#include "zgemm_NN.h"
+#include "zgemm_NT.h"
+#include "zgemm_TN.h"
+#include "zgemm_TT.h"
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup summa_dgemm
+ *
+ *  dplasma_zgemm_New - Generates the handle that performs one of the following
+ *  matrix-matrix operations. WARNING: The computations are not done by this call.
+ *
+ *    \f[ C = \alpha [op( A )\times op( B )],
+ *
+ *  where op( X ) is one of
+ *
+ *    op( X ) = X  or op( X ) = X' or op( X ) = conjg( X' )
+ *
+ *  alpha is scalar, and A, B and C are matrices, with op( A )
+ *  an m by k matrix, op( B ) a k by n matrix and C an m by n matrix.
+ *
+ *******************************************************************************
+ *
+ * @param[in] transA
+ *          Specifies whether the matrix A is transposed, not transposed or conjugate transposed:
+ *          = PlasmaNoTrans:   A is not transposed;
+ *          = PlasmaTrans:     A is transposed;
+ *          = PlasmaConjTrans: A is conjugate transposed.
+ *
+ * @param[in] transB
+ *          Specifies whether the matrix B is transposed, not transposed or conjugate transposed:
+ *          = PlasmaNoTrans:   B is not transposed;
+ *          = PlasmaTrans:     B is transposed;
+ *          = PlasmaConjTrans: B is conjugate transposed.
+ *
+ * @param[in] alpha
+ *          alpha specifies the scalar alpha
+ *
+ * @param[in] A
+ *          Descriptor of the distributed matrix A.
+ *
+ * @param[in] B
+ *          Descriptor of the distributed matrix B.
+ *
+ * @param[in] beta
+ *          beta specifies the scalar beta and is always considered null
+ *
+ * @param[out] C
+ *          Descriptor of the distributed matrix C.
+ *          On exit, the data described by C contain the matrix (
+ *          alpha*op( A )*op( B ) )
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval NULL if incorrect parameters are given.
+ *          \retval The dague handle describing the operation that can be
+ *          enqueued in the runtime with dague_enqueue(). It, then, needs to be
+ *          destroy with dplasma_zgemm_Destruct();
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgemm
+ * @sa dplasma_zgemm_Destruct
+ * @sa dplasma_cgemm_New
+ * @sa dplasma_dgemm_New
+ * @sa dplasma_sgemm_New
+ *
+ ******************************************************************************/
+dague_handle_t*
+summa_zgemm_New( PLASMA_enum transA, PLASMA_enum transB,
+                 dague_complex64_t alpha, const irregular_tiled_matrix_desc_t* A,
+                 const irregular_tiled_matrix_desc_t* B,
+                 irregular_tiled_matrix_desc_t* C)
+{
+    two_dim_block_cyclic_t *Cdist;
+    dague_handle_t* zgemm_handle;
+    dague_arena_t* arena;
+    int P, Q, m, n;
+
+    /* Check input arguments */
+    if ((transA != PlasmaNoTrans) && (transA != PlasmaTrans) && (transA != PlasmaConjTrans)) {
+        dplasma_error("dplasma_zgemm_New", "illegal value of transA");
+        return NULL /*-1*/;
+    }
+    if ((transB != PlasmaNoTrans) && (transB != PlasmaTrans) && (transB != PlasmaConjTrans)) {
+        dplasma_error("dplasma_zgemm_New", "illegal value of transB");
+        return NULL /*-2*/;
+    }
+    if ( !(C->dtype & two_dim_block_cyclic_type) ) {
+        dplasma_error("dplasma_zgemm_New", "illegal type of descriptor for C (must be two_dim_block_cyclic_t)");
+        return NULL;
+    }
+
+    P = ((two_dim_block_cyclic_t*)C)->grid.rows;
+    Q = ((two_dim_block_cyclic_t*)C)->grid.cols;
+
+    m = dplasma_imax(C->mt, P);
+    n = dplasma_imax(C->nt, Q);
+
+    /* Create a copy of the A matrix to be used as a data distribution metric.
+     * As it is used as a NULL value we must have a data_copy and a data associated
+     * with it, so we can create them here.
+     * Create the task distribution */
+    Cdist = (two_dim_block_cyclic_t*)malloc(sizeof(two_dim_block_cyclic_t));
+
+    two_dim_block_cyclic_init(
+        Cdist, matrix_RealDouble, matrix_Tile,
+        C->super.nodes, C->super.myrank,
+        1, 1, /* Dimensions of the tiles              */
+        m, n, /* Dimensions of the matrix             */
+        0, 0, /* Starting points (not important here) */
+        m, n, /* Dimensions of the submatrix          */
+        1, 1, P);
+    Cdist->super.super.data_of = NULL;
+    Cdist->super.super.data_of_key = NULL;
+
+    if( PlasmaNoTrans == transA ) {
+        if( PlasmaNoTrans == transB ) {
+            dague_zgemm_NN_handle_t* handle;
+            handle = dague_zgemm_NN_new(transA, transB, alpha, beta,
+                                        (dague_ddesc_t*)A,
+                                        (dague_ddesc_t*)B,
+                                        (dague_ddesc_t*)C,
+                                        (dague_ddesc_t*)Cdist);
+            arena = handle->arenas[DAGUE_zgemm_NN_DEFAULT_ARENA];
+            zgemm_handle = (dague_handle_t*)handle;
+        } else {
+            dague_zgemm_NT_handle_t* handle;
+            handle = dague_zgemm_NT_new(transA, transB, alpha, beta,
+                                        (dague_ddesc_t*)A,
+                                        (dague_ddesc_t*)B,
+                                        (dague_ddesc_t*)C,
+                                        (dague_ddesc_t*)Cdist);
+            arena = handle->arenas[DAGUE_zgemm_NT_DEFAULT_ARENA];
+            zgemm_handle = (dague_handle_t*)handle;
+        }
+    } else {
+        if( PlasmaNoTrans == transB ) {
+            dague_zgemm_TN_handle_t* handle;
+            handle = dague_zgemm_TN_new(transA, transB, alpha, beta,
+                                        (dague_ddesc_t*)A,
+                                        (dague_ddesc_t*)B,
+                                        (dague_ddesc_t*)C,
+                                        (dague_ddesc_t*)Cdist);
+            arena = handle->arenas[DAGUE_zgemm_TN_DEFAULT_ARENA];
+            zgemm_handle = (dague_handle_t*)handle;
+        }
+        else {
+            dague_zgemm_TT_handle_t* handle;
+            handle = dague_zgemm_TT_new(transA, transB, alpha, beta,
+                                        (dague_ddesc_t*)A,
+                                        (dague_ddesc_t*)B,
+                                        (dague_ddesc_t*)C,
+                                        (dague_ddesc_t*)Cdist);
+            arena = handle->arenas[DAGUE_zgemm_TT_DEFAULT_ARENA];
+            zgemm_handle = (dague_handle_t*)handle;
+        }
+    }
+
+    dplasma_add2arena_tile(arena,
+                           A->mb*A->nb*sizeof(dague_complex64_t),
+                           DAGUE_ARENA_ALIGNMENT_SSE,
+                           dague_datatype_double_complex_t, A->mb);
+
+    return zgemm_handle;
+}
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64
+ *
+ *  dplasma_zgemm_Destruct - Free the data structure associated to an handle
+ *  created with dplasma_zgemm_New().
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] handle
+ *          On entry, the handle to destroy.
+ *          On exit, the handle cannot be used anymore.
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgemm_New
+ * @sa dplasma_zgemm
+ *
+ ******************************************************************************/
+void
+dplasma_zgemm_Destruct( dague_handle_t *handle )
+{
+    dague_zgemm_NN_handle_t *zgemm_handle = (dague_zgemm_NN_handle_t *)handle;
+    irregular_tiled_matrix_desc_destroy( (irregular_tiled_matrix_desc_t*)(zgemm_handle->Cdist) );
+    free( zgemm_handle->Cdist );
+
+    dague_matrix_del2arena( ((dague_zgemm_NN_handle_t *)handle)->arenas[DAGUE_zgemm_NN_DEFAULT_ARENA] );
+    dague_handle_free(handle);
+}
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64
+ *
+ *  dplasma_zgemm - Performs one of the following matrix-matrix operations
+ *
+ *    \f[ C = \alpha [op( A )\times op( B )] + \beta C \f],
+ *
+ *  where op( X ) is one of
+ *
+ *    op( X ) = X  or op( X ) = X' or op( X ) = conjg( X' )
+ *
+ *  alpha and beta are scalars, and A, B and C  are matrices, with op( A )
+ *  an m by k matrix, op( B ) a k by n matrix and C an m by n matrix.
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] dague
+ *          The dague context of the application that will run the operation.
+ *
+ * @param[in] transA
+ *          Specifies whether the matrix A is transposed, not transposed or conjugate transposed:
+ *          = PlasmaNoTrans:   A is not transposed;
+ *          = PlasmaTrans:     A is transposed;
+ *          = PlasmaConjTrans: A is conjugate transposed.
+ *
+ * @param[in] transB
+ *          Specifies whether the matrix B is transposed, not transposed or conjugate transposed:
+ *          = PlasmaNoTrans:   B is not transposed;
+ *          = PlasmaTrans:     B is transposed;
+ *          = PlasmaConjTrans: B is conjugate transposed.
+ *
+ * @param[in] alpha
+ *          alpha specifies the scalar alpha
+ *
+ * @param[in] A
+ *          Descriptor of the distributed matrix A.
+ *
+ * @param[in] B
+ *          Descriptor of the distributed matrix B.
+ *
+ * @param[in] beta
+ *          beta specifies the scalar beta
+ *
+ * @param[in,out] C
+ *          Descriptor of the distributed matrix C.
+ *          On exit, the data described by C are overwritten by the matrix (
+ *          alpha*op( A )*op( B ) + beta*C )
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval -i if the ith parameters is incorrect.
+ *          \retval 0 on success.
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgemm_New
+ * @sa dplasma_zgemm_Destruct
+ * @sa dplasma_cgemm
+ * @sa dplasma_dgemm
+ * @sa dplasma_sgemm
+ *
+ ******************************************************************************/
+int
+summa_zgemm( dague_context_t *dague,
+             PLASMA_enum transA, PLASMA_enum transB,
+             dague_complex64_t alpha, const irregular_tiled_matrix_desc_t *A,
+             const irregular_tiled_matrix_desc_t *B,
+             irregular_tiled_matrix_desc_t *C)
+{
+    dague_handle_t *dague_zgemm = NULL;
+    int M, N, K;
+    int Am, An, Ai, Aj, Amb, Anb;
+    int Bm, Bn, Bi, Bj, Bmb, Bnb;
+
+    /* Check input arguments */
+    if ((transA != PlasmaNoTrans) && (transA != PlasmaTrans) && (transA != PlasmaConjTrans)) {
+        dplasma_error("summa_zgemm", "illegal value of transA");
+        return -1;
+    }
+    if ((transB != PlasmaNoTrans) && (transB != PlasmaTrans) && (transB != PlasmaConjTrans)) {
+        dplasma_error("summa_zgemm", "illegal value of transB");
+        return -2;
+    }
+
+    if ( transA == PlasmaNoTrans ) {
+        Am  = A->m;
+        An  = A->n;
+        Amb = A->mb;
+        Anb = A->nb;
+        Ai  = A->i;
+        Aj  = A->j;
+    } else {
+        Am  = A->n;
+        An  = A->m;
+        Amb = A->nb;
+        Anb = A->mb;
+        Ai  = A->j;
+        Aj  = A->i;
+    }
+
+    if ( transB == PlasmaNoTrans ) {
+        Bm  = B->m;
+        Bn  = B->n;
+        Bmb = B->mb;
+        Bnb = B->nb;
+        Bi  = B->i;
+        Bj  = B->j;
+    } else {
+        Bm  = B->n;
+        Bn  = B->m;
+        Bmb = B->nb;
+        Bnb = B->mb;
+        Bi  = B->j;
+        Bj  = B->i;
+    }
+
+    if ( (Amb != C->mb) || (Anb != Bmb) || (Bnb != C->nb) ) {
+        dplasma_error("summa_zgemm", "tile sizes have to match");
+        return -101;
+    }
+    if ( (Am != C->m) || (An != Bm) || (Bn != C->n) ) {
+        dplasma_error("summa_zgemm", "sizes of matrices have to match");
+        return -101;
+    }
+    if ( (Ai != C->i) || (Aj != Bi) || (Bj != C->j) ) {
+        dplasma_error("summa_zgemm", "start indexes have to match");
+        return -101;
+    }
+
+    if ( !(C->dtype & irregular_tiled_matrix_type) ) {
+        dplasma_error("summa_zgemm", "illegal type of descriptor for C");
+        return -3.;
+    }
+
+    M = C->m;
+    N = C->n;
+    K = An;
+
+    /* Quick return */
+    if (M == 0 || N == 0 || ((alpha == (PLASMA_Complex64_t)0.0 || K == 0))
+        return 0;
+
+    dague_zgemm = summa_zgemm_New(transA, transB,
+                                  alpha, A,
+                                  B,
+                                  C);
+
+    if ( dague_zgemm != NULL )
+    {
+        dague_enqueue( dague, (dague_handle_t*)dague_zgemm);
+        dplasma_progress(dague);
+        dplasma_zgemm_Destruct( dague_zgemm );
+        return 0;
+    }
+    else {
+        return -101;
+    }
+}
