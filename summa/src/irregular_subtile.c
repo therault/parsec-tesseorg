c76ff001c5f5d8772e02843650a34416aeb5d35d
Damien Genet <dgenet@icl.utk.edu>
2017-03-09 18:42:47 -0500

    Add support for GPU and recurrsive irregular tiling.

--- summa/src/irregular_subtile.c
+++ summa/src/irregular_subtile.c
@@ -62,8 +62,8 @@ irregular_subtile_desc_t *irregular_subtile_desc_create(const irregular_tiled_ma
     /* if (NT > 1 && Ntiling[NT-1] < bigtile) { Ntiling[NT-2] += Ntiling[NT-1]; NT--;} */
 
     fprintf(stdout, " in %d along m, in %d along n\n", MT, NT);
-    for (i = 0; i < MT; ++i) fprintf(stdout, "%s %d%s", (i == 0) ? "  -> Mtiling:" : ",", Mtiling[i], (i == MT-1) ? "\n" : "");
-    for (i = 0; i < NT; ++i) fprintf(stdout, "%s %d%s", (i == 0) ? "  -> Ntiling:" : ",", Ntiling[i], (i == NT-1) ? "\n" : "");
+    /* for (i = 0; i < MT; ++i) fprintf(stdout, "%s %d%s", (i == 0) ? "  -> Mtiling:" : ",", Mtiling[i], (i == MT-1) ? "\n" : ""); */
+    /* for (i = 0; i < NT; ++i) fprintf(stdout, "%s %d%s", (i == 0) ? "  -> Ntiling:" : ",", Ntiling[i], (i == NT-1) ? "\n" : ""); */
 
     /* Initialize the tiled_matrix descriptor */
     irregular_tiled_matrix_desc_init( &(sdesc->super), tdesc->mtype,
@@ -134,41 +134,21 @@ static uint32_t irregular_subtile_rank_of_key(parsec_ddesc_t* ddesc, parsec_data
 
 static int32_t irregular_subtile_vpid_of(parsec_ddesc_t* ddesc, ...)
 {
-<<<<<<< HEAD
-    /* int pq = vpmap_get_nb_vp(); */
-    /* if ( pq == 1 ) */
-    /*     return 0; */
-
-    /* int i, j, vpid = 0; */
-    /* (void)i; */
-    /* (void)j; */
-    /* va_list ap; */
-    /* /\* Get coordinates *\/ */
-    /* va_start(ap, ddesc); */
-    /* i = (int)va_arg(ap, unsigned int); */
-    /* j = (int)va_arg(ap, unsigned int); */
-    /* va_end(ap); */
-
-    /* return vpid; */
-
-    /* return ((irregular_subtile_desc_t*)ddesc)->vpid; */
-=======
     int pq = vpmap_get_nb_vp();
     if ( pq == 1 )
         return 0;
 
-	int i, j, vpid = 0;
-	(void)i;
-	(void)j;
-	va_list ap;
+    int i, j, vpid = 0;
+    (void)i;
+    (void)j;
+    va_list ap;
     /* Get coordinates */
     va_start(ap, ddesc);
     i = (int)va_arg(ap, unsigned int);
     j = (int)va_arg(ap, unsigned int);
     va_end(ap);
 
-	return vpid;
->>>>>>> Adds: Recursive tiling
+    return vpid;
 }
 
 static int32_t irregular_subtile_vpid_of_key(parsec_ddesc_t* ddesc, parsec_data_key_t key)
