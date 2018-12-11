3d99f4fb01e0ab5256341153a62a0600a5809416
Thomas Herault <herault@icl.utk.edu>
2018-12-11 11:53:22 -0500

    Use non-signed types for datarepo to match with atomics prototypes

--- parsec/datarepo.h
+++ parsec/datarepo.h
@@ -78,9 +78,9 @@ struct data_repo_entry_s {
     void*                      generator;
     parsec_hash_table_item_t   ht_item;
     parsec_execution_stream_t *ttg_task;
-    volatile uint32_t          usagecnt;
-    volatile uint32_t          usagelmt;
-    volatile uint32_t          retained;
+    volatile int32_t           usagecnt;
+    volatile int32_t           usagelmt;
+    volatile int32_t           retained;
 #if defined(PARSEC_SIM)
     int                        sim_exec_date;
 #endif
